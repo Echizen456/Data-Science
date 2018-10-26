3c724d84f794dc4b01ca79a46a526b2c3e7dd9d8
Echizen456 <1617655807@qq.com>
2018-10-26 18:07:18 +0800

    AVF  by R to:detect abnormal row input:csv  without header output :score for each row
    score is sum(frequncy for element in columns)/length(col)
    
    
    Signed-off-by: Echizen456 <1617655807@qq.com>

---
+++ AVF.R
@@ -0,0 +1,30 @@
+
+AVF <- function(file){
+  library(plyr);
+  mydata <- read.csv(file, header = FALSE, sep = ",",stringsAsFactors=,encoding = "utf-8")
+  #print(head(mydata))
+  row <- nrow(mydata)
+  col <- ncol(mydata)
+
+  for ( i in 1: row)
+  {
+
+    sum <- 0
+    for (j in 1:col)
+    {
+      #print(j)
+      freq_table = table(unlist(unname(mydata[,j])))
+
+      value <- paste0(mydata[i,j],"")
+
+      freq <- freq_table [value]
+      #print(paste0(value,freq))
+      sum <- sum + freq
+
+    }
+
+    mydata[i,"Score"] <- sum/col
+    mydata[i,"Score2"] <- paste0(sum,"/" ,col)
+  }
+  mydata
+}
